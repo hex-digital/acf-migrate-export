728d466efd22efbee93e0f44e49fe0ac2a28d9b7
Jamie Warburton <jamie@hexdigital.com>
2018-03-07 10:31:59 +0000

    Add the generate script

--- generate.php
+++ generate.php
@@ -4,8 +4,75 @@ require __DIR__ . '/vendor/autoload.php';
 
 use App\MigrationGenerator;
 use App\Models\Export;
+use App\Parsers;
+use Helpers;
+
+// We use a global variable because the export file calls the function
+// `acf_add_local_field_group()`. This way, we can use this to add the field
+// groups to $export without having to edit the export file in any way.
+global $export;
 
 $migrationGenerator = new MigrationGenerator();
 $export = new Export();
 
-$migrationGenerator->createMigrations($export);
+// -f: The file path of the export to migrate
+// -a: If the specified file path is absolute, this flag should be added
+$options = getopt('f:a');
+
+if (!isset($options['f'])) {
+    Helpers\ConsoleOutput::write(
+        'Please specify a file path to an export file with the -f flag',
+        Helpers\ConsoleOutput::ERROR
+    );
+    die();
+}
+
+$exportFilePath = $options['f'];
+if (!isset($options['a'])) {
+    $exportFilePath = __DIR__ . '/' . $exportFilePath;
+}
+
+if (!file_exists($exportFilePath)) {
+    Helpers\ConsoleOutput::write(
+        'Could not locate file ' . $exportFilePath . '. Add flag -a if using an absolute path',
+        Helpers\ConsoleOutput::ERROR
+    );
+    die();
+}
+
+// This file is from the ACF export, and calls the `acf_add_local_field_group`
+// function for each field group. We capture this with a helper function below
+include $exportFilePath;
+
+// Now everything will be in $export, so we can generate our migrations file
+// $migrationGenerator->createMigrations($export);
+
+
+/* Helper Functions for the ACF Export */
+
+/**
+ * Used to add a Field Group to the global export, so that a migration file can
+ * be generated from it.
+ * @param array $fieldGroupArray Array of Field Groups from ACF export
+ * @return void
+ */
+function addFieldGroupToExport(array $fieldGroupArray): void {
+    global $export;
+
+    $fieldGroupParser = new Parsers\FieldGroupParser();
+    $fieldGroup = $fieldGroupParser->parseToFieldGroup($fieldGroupArray);
+
+    // $export->addFieldGroup($fieldGroup);
+}
+
+/**
+ * Helper function - this is the function that the exported code from ACF calls
+ * for each Field Group. By using this helper function, we can simply plug the
+ * PHP ACF export straight into this code without having to modify it at all.
+ * This function simply passes along the fieldGroupArray to the real function
+ * @param  array  $fieldGroupArray Array of Field Groups from ACF export
+ * @return void
+ */
+function acf_add_local_field_group(array $fieldGroupArray): void {
+    addFieldGroupToExport($fieldGroupArray);
+}
---
+++ helpers/ConsoleOutput.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Helpers;
+
+class ConsoleOutput
+{
+    const ERROR = 'error';
+    const WARN = 'warning';
+    const SUCCESS = 'success';
+
+    const COLOR_ERROR = "\033[1;31m";
+    const COLOR_WARN = "\033[1;33m";
+    const COLOR_SUCCESS = "\033[1;32m";
+    const COLOR_RESET = "\033[0m";
+
+    public static function write(string $message, $type = null)
+    {
+        $output = '--> ';
+
+        if ($type !== null) {
+            switch ($type) {
+                case self::ERROR:
+                    $textPrefix = self::COLOR_ERROR . "Error: ";
+                    break;
+                case self::SUCCESS:
+                    $textPrefix = self::COLOR_SUCCESS . "Success: ";
+                    break;
+                case self::WARN:
+                default:
+                    $textPrefix = self::COLOR_WARN . "Warning: ";
+                    break;
+            }
+            $output .= $textPrefix . self::COLOR_RESET;
+        }
+
+        $output .= $message;
+
+        echo $output . PHP_EOL;
+    }
+}
