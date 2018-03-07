effdc2b0c619c48b71fd77a75e431b32d4d049bc
Jamie Warburton <jamie@hexdigital.com>
2018-03-07 12:19:14 +0000

    Began adding tests and implementation for FieldGroupParser

--- app/Models/FieldGroup.php
+++ app/Models/FieldGroup.php
@@ -2,7 +2,50 @@
 
 namespace App\Models;
 
+use App\Models\AbstractField;
+
 class FieldGroup
 {
+    protected $key;
+    protected $title;
+    protected $fields;
+    protected $options;
 
+    const DEFAULT_OPTIONS = [];
+
+    public function __construct(
+        string $key,
+        string $title,
+        array $fields,
+        array $options
+    ) {
+        $this->key = $key;
+        $this->title = $title;
+        $this->fields = $fields;
+        $this->options = $options;
+    }
+
+    public function getKey(): string {
+        return $this->key;
+    }
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function getFields(): array {
+        return $this->fields;
+    }
+
+    public function addField(AbstractField $field): void {
+        $this->fields[] = $field;
+    }
+
+    public function getOptions(): array {
+        return $this->options;
+    }
+
+    public function addOption(string $key, $value): void {
+        $this->options[$key] = $value;
+    }
 }
--- app/Parsers/FieldGroupParser.php
+++ app/Parsers/FieldGroupParser.php
@@ -2,7 +2,24 @@
 
 namespace App\Parsers;
 
+use App\Models\FieldGroup;
+use App\Models\AbstractField;
+
 class FieldGroupParser
 {
+    /**
+     * Create a FieldGroup object from an Array of field group data.
+     * This also turns all contained field data into AbstractField objects.
+     * @param  array  $fieldGroupArray
+     * @return FieldGroup
+     */
+    public function parseToFieldGroup(array $fieldGroupArray): FieldGroup
+    {
+        $key = $fieldGroupArray['key'];
+        $title = $fieldGroupArray['title'];
+        $fields = [];
+        $options = [];
 
+        return new FieldGroup($key, $title, $fields, $options);
+    }
 }
